940f7890bd767b596993c19b4a193eb5919d5e52
fvendrasco <felipe@vendrasco.com.br>
2021-01-25 14:16:41 -0300

    push to develop

--- MoedaDigital/View/TabBarViewController.swift
+++ MoedaDigital/View/TabBarViewController.swift
@@ -16,18 +16,6 @@ class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let tableViewMoeda = moreNavigationController.viewControllers.first?.view as? UITableView
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
