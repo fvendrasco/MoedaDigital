69ba549d9d102474a0fea10c75935479fd79d1e1
fernandaandreia <fernanda.nascimento.btk@gmail.com>
2021-01-25 11:48:31 -0300

    teste

--- MoedaDigital/Application/AppDelegate.swift
+++ MoedaDigital/Application/AppDelegate.swift
@@ -138,7 +138,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
         */
-        let container = NSPersistentContainer(name: "EverisFilmes")
+        let container = NSPersistentContainer(name: "MoedsDigital")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
---
+++ MoedaDigital/View/TabBarViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TabBarViewController.swift
+//  MoedaDigital
+//
+//  Created by Felipe Augusto Vendrasco on 25/01/21.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+ 
+    @IBOutlet weak var buttonMoedasTabBar: UIViewController!
+    
+    @IBOutlet weak var buttonFavoritosTabBar: UIViewController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    
+}
