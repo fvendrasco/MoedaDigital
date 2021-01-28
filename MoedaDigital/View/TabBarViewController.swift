98377f5cbbb3b80650fe241e3781fe6d9824f19b
fvendrasco <felipe@vendrasco.com.br>
2021-01-28 09:25:31 -0300

    alteração info.plist

--- MoedaDigital/View/TabBarViewController.swift
+++ MoedaDigital/View/TabBarViewController.swift
@@ -22,17 +22,6 @@ class TabBarViewController: UITabBarController {
         fatalError("init(coder:) has not been implemented")
     }
     
-   
-    
-    init(){
-        
-        super.init(nibName: TabBarViewController.description(), bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tabBarMoedas()
